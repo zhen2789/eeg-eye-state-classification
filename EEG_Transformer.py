d16bb1b05f7e2a6111ce7b47573ee3b6ea86baaf
Gary Zhen <garyzhen7@gmail.com>
2026-06-30 09:46:02 -0400

    Update EEG_Transformer.py

--- EEG_Transformer.py
+++ EEG_Transformer.py
@@ -90,23 +90,6 @@ def estimate_loss():
     model.train()
     return out
 
-class LayerNorm1d:
-
-    def __init__(self, dim, eps=1e-5, momentum=0.1):
-        self.eps = eps
-        self.gamma = torch.ones(dim)
-        self.beta = torch.zeros(dim)
-
-    def __call__(self, x):
-        xmean = x.mean(1, keepdim=True)
-        xvar = x.var(1, keepdim=True)
-        xhat = (x - xmean) / torch.sqrt(xvar + self.eps)
-        self.out = self.gamma * xhat + self.beta
-        return self.out
-
-    def parameters(self):
-        return [self.gamma, self.beta]
-
 class RoPE(nn.Module):
 
     def __init__(self, d, max_seq_len):
